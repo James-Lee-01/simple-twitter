f169eadc9fef0efa6eadbe924b4e17978068eab6
James-Lee-01 <freebike01@gmail.com>
2023-08-25 01:47:47 +0800

    feat: add Link

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,5 +1,7 @@
 import styles from './LoginPage.module.scss'
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import AuthPageContainer from '../../components/AuthPageContainer/AuthPageContainer.jsx'
 import AuthInput from '../../components/AuthInput/AuthInput.jsx'
 import Button from '../../components/Button/Button.jsx'
 
@@ -10,32 +12,39 @@ function LoginPage() {
   const [password, setPassword] = useState('')
 
   return (
-    <>
-    <AuthInput
-      labelName='帳號'
-      type='text'
-      value={account}
-      placeholder='請輸入帳號'
-      onChange={(accountInput) => setAccount(accountInput)}
-      notification='字數超出上限'
-      lengthLimit={50}
-    />
-    <AuthInput
-      labelName='密碼'
-      type='password'
-      value={password}
-      placeholder='請輸入密碼'
-      onChange={(passwordInput) => setPassword(passwordInput)}
-      notification='字數超出上限'
-      lengthLimit={5}
-    />
-    <Button size='extraLarge' title='登入' />
-    <div className={styles.link}>
-      <Link></Link>
-      <Link></Link>
-    </div>
-    </>
-  )
+    <AuthPageContainer title='登入 Alphitter'>
+      <AuthInput
+        labelName='帳號'
+        type='text'
+        value={account}
+        placeholder='請輸入帳號'
+        onChange={(accountInput) => setAccount(accountInput)}
+        notification='字數超出上限'
+        lengthLimit={50}
+      />
+      <AuthInput
+        labelName='密碼'
+        type='password'
+        value={password}
+        placeholder='請輸入密碼'
+        onChange={(passwordInput) => setPassword(passwordInput)}
+        notification='字數超出上限'
+        lengthLimit={5}
+      />
+      <Button size='extraLarge' title='登入' />
+
+      {/* switch link */}
+      <div className={styles.linkContainer}>
+        <Link to='/signup'>
+          <span className={styles.link}>註冊</span>
+        </Link>
+        <span>・</span>
+        <Link to='/admin/login'>
+          <span className={styles.link}>後台登入</span>
+        </Link>
+      </div>
+    </AuthPageContainer>
+  );
 }
 
 export default LoginPage
