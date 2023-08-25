57b2ca686f707589ee09a2b721024a6dc4377e28
James-Lee-01 <freebike01@gmail.com>
2023-08-26 01:50:04 +0800

    feat: add admin login

--- src/api/auth.js
+++ src/api/auth.js
@@ -12,7 +12,7 @@ const dummyData = [
 ]
 
 //api
-//login
+//login(user only)
 export const userLogin = async ({ account, password }) => {
   try {
     const { data } = await axios.get(dummyData, {
@@ -38,6 +38,53 @@ export const userLogin = async ({ account, password }) => {
     return data
   } catch (error) {
     console.error(`[Get User failed]: `, error)
+    return error
+  }
+}
+
+//login(admin only)
+export const adminLogin = async ({ account, password }) => {
+  try {
+    const { data } = await axios.get(dummyData, {
+      account,
+      password,
+    })
+    // const { data } = await axios.get(`${baseUrl}/admin/login`, {
+    //   account,
+    //   password,
+    // })
+
+    //驗證角色身份
+    const { authToken, role } = data 
+    //若角色為admin管理者
+    if (authToken && role === 'admin') {
+      return { success: true, ...data }
+    }
+    //若角色符合user
+    if (authToken && role === 'user') {
+      //顯示錯誤訊息(暫時)
+      console.error('帳號不存在')
+    }
+    return data
+  } catch (error) {
+    console.error(`[Get User failed]: `, error)
+    return error
+  }
+}
+
+//signup(user)
+export const userSignUp = async ({ account, name, email, password, passwordCheck }) => {
+  try {
+    const { data } = await axios.post(`${baseUrl}/users`, {
+      account,
+      name,
+      email,
+      password,
+      passwordCheck,
+    })
+    return data
+  } catch (error) {
+    console.error('[SignUp Failed]', error)
     return error
   }
 }
