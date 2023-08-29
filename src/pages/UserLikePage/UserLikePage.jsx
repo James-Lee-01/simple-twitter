7bffd399acb3bf333b99812ef5d40a80bc2c6703
Ya-Yun-Zheng <nmnm741852963@gmail.com>
2023-08-29 17:52:51 +0800

    feat:UserLikePage.UserReplyPage

--- src/pages/UserLikePage/UserLikePage.jsx
+++ src/pages/UserLikePage/UserLikePage.jsx
@@ -1,49 +1,49 @@
-// import style from "./UserLikePage.module.scss";
-// import Navbar from "../../components/Main/Navbar/Navbar";
-// import MainLayout from "../../components/Main/MainLayout/MainLayout";
-// import UserInfo from "../../components/Main/Navbar/UserInfo/UserInfo";
-// import CurrentUser from "../../components/Main/CurrentUser/CurrentUser";
-// import TweetItem from "../../components/Main/TweetItem/TweetItem";
-// import UserToggleMenu from "../../components/Main/UserToggleMenu/UserToggleMenu";
-// import { useParams } from "react-router-dom";
-// const userInfo = {
-//   "name": "John Doe",
-//   "account": "heyJohn",
-//   "following": "34",
-//   "follower": "59",
-// }
+import style from "./UserLikePage.module.scss";
+import Navbar from "../../components/Main/Navbar/Navbar";
+import MainLayout from "../../components/Main/MainLayout/MainLayout";
+import UserInfo from "../../components/Main/Navbar/UserInfo/UserInfo";
+import CurrentUser from "../../components/Main/CurrentUser/CurrentUser";
+import TweetItem from "../../components/Main/TweetItem/TweetItem";
+import UserToggleMenu from "../../components/Main/UserToggleMenu/UserToggleMenu";
+import { useParams } from "react-router-dom";
+const userInfo = {
+  "name": "John Doe",
+  "account": "heyJohn",
+  "following": "34",
+  "follower": "59",
+}
 
-// const tweetsList = [
-//   {
-//     "name": "John Doe",
-//     "account": "heyJohn",
-//     "time": "3 小時",
-//     "replyCount": "13",
-//     "likeCount": "76"
-//   },
-// ]
+const tweetsList = [
+  {
+    "name": "John Doe",
+    "account": "heyJohn",
+    "time": "3 小時",
+    "replyCount": "13",
+    "likeCount": "76"
+  },
+]
 
-// const UserLikePage = () => {
-//   const { userId } = useParams();
+const UserLikePage = () => {
+  const { userId } = useParams();
 
-//   const linkList = [
-//     { title: "推文", link: `/user/${userId}/tweet` },
-//     { title: "推文與回覆", link: `/user/${userId}/reply` },
-//     { title: "喜歡的內容", link: `/user/${userId}/like` },
-//   ];
+  const linkList = [
+    { title: "推文", link: `/user/${userId}/tweet` },
+    { title: "推文與回覆", link: `/user/${userId}/reply` },
+    { title: "喜歡的內容", link: `/user/${userId}/like` },
+  ];
 
-//   return <MainLayout>
-//     <Navbar hasBack={true} >
-//       <UserInfo />
-//     </Navbar>
-//     <CurrentUser user={userInfo} />
-//     <UserToggleMenu linkList={linkList} />
-//     <div className={style.tweetList}>
-//       {
-//         Array.from(Array(16)).map((_, index) => <TweetItem user={likeTweetList[0]} />)
-//       }
-//     </div>
-//   </MainLayout>
-// }
+  return <MainLayout>
+    <Navbar hasBack={true} >
+      <UserInfo />
+    </Navbar>
+    <CurrentUser user={userInfo} />
+    <UserToggleMenu linkList={linkList} />
+    {/* <div className={style.tweetList}>
+      {
+        Array.from(Array(16)).map((_, index) => <TweetItem user={likeTweetList[0]} />)
+      }
+    </div> */}
+  </MainLayout>
+}
 
-// export default UserLikePage;
+export default UserLikePage;
