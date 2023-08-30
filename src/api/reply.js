355e7e3b722551ff184d891970bcca34b2192678
Ya-Yun-Zheng <nmnm741852963@gmail.com>
2023-08-31 00:04:46 +0800

    tweetreply

--- src/api/reply.js
+++ src/api/reply.js
@@ -1,15 +1,28 @@
 import axios from "axios";
 import Swal from 'sweetalert2';
+import { apiHelper } from '../api/tweet.js';
 
 export const baseUrl = ""
-const axiosInstance = axios.create({
-  baseURL: baseUrl,
-});
+// const axiosInstance = axios.create({
+//   baseURL: baseUrl,
+// });
+
+// get tweet replies
+export const getTweetReplies = async (tweetId) => {
+  try {
+    const { data } = await apiHelper.get(`/tweets/${tweetId}/replies`);
+
+    return data;
+  } catch (error) {
+    console.error("[Get tweet replies Failed]:", error);
+    return error
+  }
+};
 
 // 發布回覆
 export const postReply = async (comment, tweetId) => {
   try {
-    const { data } = await axiosInstance.post(`/tweets/${tweetId}/replies`, {
+    const { data } = await apiHelper.post(`/tweets/${tweetId}/replies`, {
       comment,
     });
 
